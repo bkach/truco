7e5cc29740b9157ce2b2f8518ac51295459d6ceb
Boris Kachscovsky <kachscovsky@gmail.com>
2020-05-07 23:05:39 +0100

    Add new endpoint to delete game

---
+++ handlers/deleteGame.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"github.com/bkach/truco-backend/handlers/util"
+	"github.com/bkach/truco-backend/truco"
+	"net/http"
+	"net/url"
+)
+
+// Handler which ...
+func DeleteGameHandler() http.HandlerFunc {
+	return util.BuildHandler(nil, func(w http.ResponseWriter, queries url.Values) {
+		gameId := queries["game_id"][0]
+
+		err := truco.DeleteGame(gameId)
+
+		if err != nil {
+			util.LogInternalError(w, err)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+}
