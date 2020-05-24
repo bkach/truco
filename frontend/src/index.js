4bd5e8af5901377ea8cd813a42ef8b7e398683e1
Ethel Ng <ethel.ng@farewill.com>
2020-05-24 21:43:05 +0100

    chore: Scaffold Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from 'state/reducer';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
