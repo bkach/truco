a9cedec9bcbfa0b536239db2ab12e144649b81b4
Ethel Ng <ethel.ng@farewill.com>
2020-05-24 22:20:31 +0100

    chore: Scaffold redux thunk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import rootReducer from 'state/reducer';
+import store from 'state/store';
 import App from 'components/App';
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
   <Provider store={store}>
